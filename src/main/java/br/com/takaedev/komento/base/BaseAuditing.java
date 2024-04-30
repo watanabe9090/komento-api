d24d6eb202e8c667f4ac7abca5873b24704d0acd
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2024-04-30 18:19:29 -0300

    :bug: Fix auditing fields on JPA

--- src/main/java/br/com/takaedev/komento/base/BaseAuditing.java
+++ src/main/java/br/com/takaedev/komento/base/BaseAuditing.java
@@ -1,10 +1,12 @@
 package br.com.takaedev.komento.base;
 
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@MappedSuperclass
 public class BaseAuditing {
     private String createdBy;
     private String modifiedBy;
